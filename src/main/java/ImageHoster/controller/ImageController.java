424248a3f4fe8f828019ff5fa9e25f963f515ed4
Shivam Runthala <runthalashivam@gmail.com>
2020-10-26 15:59:11 +0530

    Added comments object in model to display on image page and uncommented the comments tests.

--- src/main/java/ImageHoster/controller/ImageController.java
+++ src/main/java/ImageHoster/controller/ImageController.java
@@ -50,6 +50,7 @@ public class ImageController {
         Image image = imageService.getImage(id);
         model.addAttribute("image", image);
         model.addAttribute("tags", image.getTags());
+        model.addAttribute("comments", image.getComments());
         return "images/image";
     }
 
@@ -100,12 +101,14 @@ public class ImageController {
             String tags = convertTagsToString(image.getTags());
             model.addAttribute("image", image);
             model.addAttribute("tags", tags);
+            model.addAttribute("comments", image.getComments());
             return "images/edit";
         } else {
             String error = "Only the owner of the image can edit the image";
             model.addAttribute("editError", error);
             model.addAttribute("image", image);
             model.addAttribute("tags", image.getTags());
+            model.addAttribute("comments", image.getComments());
             return "images/image";
         }
     }
@@ -161,6 +164,7 @@ public class ImageController {
             model.addAttribute("deleteError", error);
             model.addAttribute("image", image);
             model.addAttribute("tags", image.getTags());
+            model.addAttribute("comments", image.getComments());
             return "images/image";
         }
     }
